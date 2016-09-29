ec5c1f0772439fc9758439f38b5dc7b7a9d2c4be
Alec Doran-Twyford <alec@alectronic.co>
2016-09-29 21:45:46 +0100

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,56 @@
+
+# System.out.print("Hello World")
+print("Hello World")
+
+x = 1
+
+# It Statment
+if x == 1:
+    print("X = 1")
+else:
+    print("X != 1")
+
+# Int
+# int myInt = 7
+myInt = 7
+
+# Float
+# double = 7.0
+myFloat = 7.0
+myFloat = float(7)
+
+# Boolean
+# boolean myBoolean = true
+myBoolean = True
+
+# String
+# String myString = "Hello World"
+myString = "Hello World"
+
+one = 1
+two = 2
+three = one + two
+
+print(three)
+
+hello = "Hello"
+world = "World"
+helloWorld = hello + " " + world
+
+print(helloWorld)
+
+a, b = 3, 4
+
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+
+print(myList[0])
+print(myList[1])
+print(myList[2])
+
+for i in myList:
+    print(i)
+
+
